0c1ab89f96beb3721a61c2b4ab0bdb17d7ea150c
iMRAN143307 <imranmawji2@gmail.com>
2026-05-24 14:07:11 -0400

    added dragging

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import random
 
 import pygame
 
@@ -12,6 +13,11 @@ playlist = [song0, song1]
 pygame.mixer.music.load(song0)
 pygame.mixer.music.play()
 SONG_END = pygame.USEREVENT + 1
+DRAG_MOUSE = pygame.USEREVENT + 2
+CUSTOMER_LOSE = pygame.USEREVENT + 3
+CUSTOMER_WIN = pygame.USEREVENT + 4
+CHECK = pygame.USEREVENT + 5
+SUBMIT = pygame.USEREVENT + 6
 pygame.mixer.music.set_endevent(SONG_END)
 width, height = 1280, 720
 screen = pygame.display.set_mode((width, height))
@@ -124,12 +130,21 @@ async def main():
         [chili_flakes, lemon, guacamole, soy_sauce, honey],
         [arrabbiata_sauce, shredded_beef, cowboy_candy, radicchio_cream, soy_pickle],
     ]
+    all_customers = [
+        ["Sweet with a little kick", 60, 30, 20, 20, 20, 2],
+        ["As bitter as their mood", 0, 20, 70, 20, 20, 0],
+        ["Salty as the sea, with no spice", 10, 20, 20, 65, 35, -1],
+        ["All in on savoury and spicy", 25, 20, 20, 25, 60, 3],
+        ["Sour with a pleasant aftertaste", 40, 70, 20, 20, 40, 0],
+    ]
+    current_customer = all_customers[random.randint(0, 4)]
     main_cooking_area = [(440, 300), (840, 700)]
     food_in_cooking_area = []
     cycle_food = int(0)
     foods_shown = all_foods[cycle_food]
     holding_item = False
     running = True
+    mouse_released = True
 
     while running:
         mouse_pos = pygame.mouse.get_pos()
@@ -137,19 +152,17 @@ async def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                if (mouse_pos[0] - 610) ** 2 + (mouse_pos[1] - 480) ** 2 <= 210**2:
+                    mouse_released = True
+                    holding_item = None
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    if holding_item:
-                        if (
-                            mouse_pos[0] >= main_cooking_area[0][0]
-                            and mouse_pos[0] <= main_cooking_area[1][0]
-                        ):
-                            if (
-                                mouse_pos[1] >= main_cooking_area[0][1]
-                                and mouse_pos[1] <= main_cooking_area[1][1]
-                            ):
-                                food_in_cooking_area.append((holding_item, mouse_pos))
-                                holding_item = False
+                    if (mouse_pos[0] - 610) ** 2 + (mouse_pos[1] - 480) ** 2 <= 210**2:
+                        if holding_item is not None:
+                            food_in_cooking_area.append((holding_item, mouse_pos))
+                        mouse_released = False
+                        pygame.time.set_timer(DRAG_MOUSE, 25)
                     if mouse_pos[1] < 237 and mouse_pos[1] > 35:
                         for i, box in enumerate(food_positions):
                             if mouse_pos[0] < box[0] + 201 and mouse_pos[0] >= box[0]:
@@ -167,6 +180,66 @@ async def main():
                 current_index = (current_index + 1) % len(playlist)
                 pygame.mixer.music.load(playlist[current_index])
                 pygame.mixer.music.play()
+            if event.type == DRAG_MOUSE:
+                if (mouse_pos[0] - 610) ** 2 + (mouse_pos[1] - 480) ** 2 <= 210**2:
+                    if not mouse_released:
+                        if holding_item is not None:
+                            food_in_cooking_area.append((holding_item, mouse_pos))
+                        pygame.time.set_timer(DRAG_MOUSE, 25)
+            if event.type == SUBMIT:
+                pass
+            if event.type == CHECK:
+                sweet = [0, 0]
+                sour = [0, 0]
+                bitter = [0, 0]
+                salty = [0, 0]
+                savoury = [0, 0]
+                spicy = [0, 0]
+                for obj in food_in_cooking_area:
+                    obj = obj[0]
+                    sweet[0] += obj[2]
+                    sweet[1] += 1
+                    sour[0] += obj[3]
+                    sour[1] += 1
+                    bitter[0] += obj[4]
+                    bitter[1] += 1
+                    salty[0] += obj[5]
+                    salty[1] += 1
+                    savoury[0] += obj[6]
+                    savoury[1] += 1
+                    spicy[0] += obj[7]
+                    spicy[1] += 1
+                sweet = sweet[0] // sweet[1]
+                sour = sour[0] // sour[1]
+                bitter = bitter[0] // bitter[1]
+                salty = salty[0] // salty[1]
+                savoury = savoury[0] // savoury[1]
+                spicy = round(spicy[0] / spicy[1])
+                flavour_profile = [sweet, sour, bitter, salty, savoury, spicy]
+                if flavour_profile[0] > current_customer[1] + 10:
+                    print("too sweet")
+                elif flavour_profile[0] < current_customer[1] - 10:
+                    print("not sweet enough")
+                if flavour_profile[1] > current_customer[2] + 10:
+                    print("too sour")
+                elif flavour_profile[1] < current_customer[2] - 10:
+                    print("not sour enough")
+                if flavour_profile[2] > current_customer[3] + 10:
+                    print("too bitter")
+                elif flavour_profile[2] < current_customer[3] - 10:
+                    print("not bitter enough")
+                if flavour_profile[3] > current_customer[4] + 10:
+                    print("too salty")
+                elif flavour_profile[3] < current_customer[4] - 10:
+                    print("not salty enough")
+                if flavour_profile[4] > current_customer[5] + 10:
+                    print("too savoury")
+                elif flavour_profile[4] < current_customer[5] - 10:
+                    print("not savoury enough")
+                if flavour_profile[5] > current_customer[6] + 1:
+                    print("too spicy")
+                elif flavour_profile[5] < current_customer[6] - 1:
+                    print("not spicy enough")
         screen.blit(background, (0, 0))
         screen.blit(taco, main_cooking_area[0])
         screen.blit(arrow_left, (0, 62))
